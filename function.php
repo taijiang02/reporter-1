5e85e0ac9e3adf4803257c8cce75fb98d3fc0ef0
taijiang02 <taijiang02@gmail.com>
2017-12-02 09:38:14 +0800

    password修改

--- function.php
+++ function.php
@@ -2,7 +2,7 @@
 //連線到資料庫
 function link_db()
 {
-    $db = new mysqli('localhost', 'root', 'happy', 'reporter');
+    $db = new mysqli('localhost', 'root', '12345', 'reporter');
     if ($db->connect_error) {
         die('無法連上資料庫：' . $db->connect_error);
     }
